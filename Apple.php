c15447ee7e47b6cecd4488c6e787151fb8df0db1
Jason Freedman <jason.freedman@blujaysolutions.com>
2020-08-27 17:22:26 -0400

    changes

--- Apple.php
+++ Apple.php
@@ -244,7 +244,7 @@ function appleSubMenu(){
 
         <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
         <div class="submenu-heading">
-            <label style="margin-bottom: 10.4px;display: inline-block;">Unlock</label>
+            <label style="margin-bottom: 11.4px;display: inline-block;">Unlock</label>
             <input style="margin-bottom: 7.4px;"  id="switch" type="checkbox"/>
             <h1 class="submenu-heading1">
                 iPhone
