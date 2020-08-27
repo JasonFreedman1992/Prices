4986d402a48ec3724159d614974043472c78947d
Jason Freedman <jason.freedman@blujaysolutions.com>
2020-08-27 16:08:37 -0400

    first commit

--- Apple.php
+++ Apple.php
@@ -194,7 +194,6 @@ function appleSubMenu(){
     <style>
 
         input#switch{
-            float: left;
             display: inline-block;
         }
 
@@ -246,7 +245,7 @@ function appleSubMenu(){
         <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
         <div class="submenu-heading">
 
-            <input id="switch" type="checkbox"/>Remember me</label>
+            <input id="switch" type="checkbox"/>
             <h1 class="submenu-heading1">
                 iPhone
             </h1>
