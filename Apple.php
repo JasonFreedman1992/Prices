ad0e25c709a64b5a6f27c00d3c41c67a0e5461d3
Jason Freedman <jason.freedman@blujaysolutions.com>
2020-08-28 14:54:54 -0400

    changes'

--- Apple.php
+++ Apple.php
@@ -333,7 +333,7 @@ function appleSubMenu(){
    
     <form method="post"> 
         <input type="submit" name="iphone-save" id="iphone-save" style="padding:10px; margin-bottom: 7.4px; margin-left: 20px; " class="pure-button pure-button-primary"/>
-            </form> -->
+            
         </div>
         <!-- <form method="post"> 
         <input type="submit" name="iphone-save" id="iphone-save" style="padding:10px; margin-bottom: 7.4px; margin-left: 20px; " class="pure-button pure-button-primary"/> -->
