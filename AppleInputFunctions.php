3d262f589e8c8f66252e2db01af3658dccadc5b1
Jason Freedman <jason.freedman@blujaysolutions.com>
2020-08-28 16:17:12 -0400

    changes

--- AppleInputFunctions.php
+++ AppleInputFunctions.php
@@ -1,14 +1,10 @@
 <?php  
-    if(isset($_POST['iphone11screenrepair'])){
-        $iphone11screenrepairprice = $_POST['iphone11screenrepair'];
-    }
-    // $iphone11screenrepairprice = $_POST['iphone11screenrepair'];
-    // global $wpdb;
+
     if(isset($_POST['iphone-save'])) { 
 
         if($iphone11screenrepair !== $_POST['iphone11screenrepair']){
             $iphone11screenrepair = $_POST['iphone11screenrepair'];
-            $price = $wpdb->get_results("UPDATE `appleiphone` SET `screenrepair`=$iphone11screenrepairprice WHERE `model`='11'");
+            $price = $wpdb->get_results("UPDATE `appleiphone` SET `screenrepair`=$iphone11screenrepair WHERE `model`='11'");
         }
         
 
